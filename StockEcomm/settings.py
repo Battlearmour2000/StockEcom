a2e53cb922e695b523ecb6c35abebbf6d4bf396f
Fredy German <fredgerman30@gmail.com>
2024-10-11 06:40:25 +0300

    fix : getting db host and port from env

--- StockEcomm/settings.py
+++ StockEcomm/settings.py
@@ -112,27 +112,27 @@ WSGI_APPLICATION = 'StockEcomm.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': '5432',  # Default to port 5432
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'Ecomm2'),
-        'USER': os.environ.get('DB_USER', 'MainAdmin'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'admin'),
-        'HOST': 'db', #change to db when in docker
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME', 'Ecomm2'),
+#         'USER': os.environ.get('DB_USER', 'MainAdmin'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD', 'admin'),
+#         'HOST': 'db', #change to db when in docker
+#         'PORT': '5432',
+#     }
+# }
 
 
 
